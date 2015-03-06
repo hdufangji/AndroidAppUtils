747d7ce90fc4d7eafc6acc5f0fac74a2da4cb7e0
hdufangji <hdu_fangji@163.com>
2015-03-06 17:34:26 +0800

    add FileModificationService

--- AndroidAppUtils/src/com/victorFun/androidapputils/test/FileTestActivity.java
+++ AndroidAppUtils/src/com/victorFun/androidapputils/test/FileTestActivity.java
@@ -13,31 +13,24 @@ import android.widget.TextView;
 import com.victorFun.androidapputils.R;
 import com.victorFun.androidapputils.file.SimpleFileObserver;
 import com.victorFun.androidapputils.file.SimpleFileObserver.onFileChangedListener;
+import com.victorFun.androidapputils.file.service.FileModificationService;
 import com.victorFun.androidapputils.fundation_technoligy.android.pair.PairDemonActivity;
 
 public class FileTestActivity extends Activity implements onFileChangedListener {
 	static final String LOG_TAG = FileTestActivity.class.getSimpleName();
-	static SimpleFileObserver fileObserver = null;
+
 	TextView tv;
 	StringBuffer sb = new StringBuffer();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		final boolean customTitleSupported = requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
 		setContentView(R.layout.only_text_view);
-		if (customTitleSupported) {
-			getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.title_bar);
-		}
 		
 		tv = (TextView)findViewById(R.id.tv);
 		tv.setText(Environment.getExternalStorageDirectory().toString());
 		
-		if(fileObserver == null){
-			fileObserver = new SimpleFileObserver(Environment.getExternalStorageDirectory().toString()+"/");			
-		}
-		fileObserver.setOnFileChangedListener(this);
-		fileObserver.startWatching();
+		startService(new Intent(this, FileModificationService.class));
 	}
 	
 	@Override
@@ -62,47 +55,7 @@ public class FileTestActivity extends Activity implements onFileChangedListener 
 
 	@Override
 	protected void onDestroy() {
-		fileObserver.stopWatching();
+		stopService(new Intent(this, FileModificationService.class));
 		super.onDestroy();
-	}
-	
-	// Options menu
-	private static final int MENU_PREFERENCES    = Menu.FIRST + 0;
-	private static final int MENU_RECONNECT_WIFI = Menu.FIRST + 1;
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		super.onCreateOptionsMenu(menu);
-		menu.add(0, MENU_PREFERENCES,    0, "123")
-				.setIcon(android.R.drawable.ic_menu_preferences);
-//		menu.add(0, MENU_RECONNECT_WIFI, 0, R.string.menu_reconnect_wifi)
-//				.setIcon(android.R.drawable.ic_menu_revert);
-		return true;
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case MENU_PREFERENCES:
-			{
-				Intent intent = new Intent(
-						this, 
-						PairDemonActivity.class
-						);
-				startActivity(intent);
-			}
-			break;
-
-//		case MENU_RECONNECT_WIFI:
-//			if (mServiceStatus.isServerRunning(mService)) {
-//				try {
-//					mService.reconnectWiFi();
-//				} catch (RemoteException e) {
-//					Log.e(LOG_TAG, e.toString());
-//				}
-//			}
-//			break;
-		}
-		
-		return super.onOptionsItemSelected(item);
 	}
 }
