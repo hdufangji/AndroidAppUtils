1428d32b9d82db168636a7a26d1637cc835356b4
vic_ <38284807@qq.com>
2015-04-20 22:24:26 +0800

    handleDemon

--- AndroidAppUtils/src/com/victor_fun/android_app_utils/fundation_technoligy/android/HandlerDemon.java
+++ AndroidAppUtils/src/com/victor_fun/android_app_utils/fundation_technoligy/android/HandlerDemon.java
@@ -1,7 +1,56 @@
 package com.victor_fun.android_app_utils.fundation_technoligy.android;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 
 public class HandlerDemon extends Activity {
+	
+	private Handler mHandler = new Handler(){
+
+		@Override
+		public void handleMessage(Message msg) {
+			// TODO Auto-generated method stub
+			super.handleMessage(msg);
+		}
+		
+	};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+	
+	private Thread mThread = new Thread(){
+		Looper looper;
+		
+		@Override
+		public void run() {
+			looper = Looper.
+			
+		}
+		
+	};
 
 }
