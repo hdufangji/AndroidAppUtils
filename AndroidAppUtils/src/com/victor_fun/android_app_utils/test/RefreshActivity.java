b473f6c1ceefa4be926d187b3ee219e8f26945ac
hdufangji <hdu_fangji@163.com>
2015-07-17 09:53:49 +0800

    add refresh demo

---
+++ AndroidAppUtils/src/com/victor_fun/android_app_utils/test/RefreshActivity.java
@@ -0,0 +1,17 @@
+package com.victor_fun.android_app_utils.test;
+
+import com.victor_fun.android_app_utils.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RefreshActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.refresh_activity);
+	}
+
+}
